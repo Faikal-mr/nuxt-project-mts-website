7a6a3a4e06da2d8b46e31569f55fc864adc68bdf
Faikal Miftahul <faikalmiftahrizki@gmail.com>
2026-03-19 23:50:58 +0700

    fix api filename

--- server/api/create-payment.post.ts
+++ server/api/create-payment.post.ts
@@ -1,24 +1,35 @@
 import midtransClient from 'midtrans-client'
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
+  try {
+    const body = await readBody(event)
 
-  const snap = new midtransClient.Snap({
-    isProduction: false,
-    serverKey: process.env.MIDTRANS_SERVER_KEY
-  })
+    const snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: process.env.MIDTRANS_SERVER_KEY
+    })
 
-  const transaction = await snap.createTransaction({
-    transaction_details: {
-      order_id: 'ORDER-' + Date.now(),
-      gross_amount: 10000
-    },
-    customer_details: {
-      email: body.email
+    console.log('SERVER KEY:', process.env.MIDTRANS_SERVER_KEY)
+
+    const transaction = await snap.createTransaction({
+      transaction_details: {
+        order_id: 'ORDER-' + Date.now(),
+        gross_amount: 10000
+      },
+      customer_details: {
+        email: body.email
+      }
+    })
+
+    return {
+      token: transaction.token
     }
-  })
+  } catch (error) {
+    console.error('MIDTRANS ERROR:', error)
 
-  return {
-    token: transaction.token
+    return {
+      error: true,
+      message: error.message
+    }
   }
 })
